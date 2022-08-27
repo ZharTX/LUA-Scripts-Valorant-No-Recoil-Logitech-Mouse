821965c89ae25093d37ef6dca2890df87b6c20ef
ZharTX <107768580+ZharTX@users.noreply.github.com>
2022-08-27 11:02:40 +0700

    Create world.lua

---
+++ world.lua
@@ -0,0 +1,27 @@
+--| ZharGX |--
+-----------------------------------| VALORANT |----------------------------------
+--| How to Activate it : Hold Backward Side Button + Left Click               |--
+---------------------------------------------------------------------------------
+--| Activate this macro while in game if your cursor moves down it means this |--
+--| macro is working on your PC.                                              |--
+---------------------------------------------------------------------------------
+
+if (event == "PROFILE_ACTIVATED") then end
+EnablePrimaryMouseButtonEvents(true)
+function OnEvent(event, arg)
+  if IsMouseButtonPressed(4) then
+    repeat
+      if IsMouseButtonPressed(1) then
+        repeat
+        MoveMouseRelative(0, 2)
+        Sleep(1)
+        until not IsMouseButtonPressed(1)
+      end
+    until not IsMouseButtonPressed(1)
+  end
+end
+
+--| If this macro works on your pc, you can buy the paid version
+--| Contact Me for Purchase VIP Macro 99% No Recoil
+--| Instagram: @ZharGX
+--| My Website: https://zhargx.com
