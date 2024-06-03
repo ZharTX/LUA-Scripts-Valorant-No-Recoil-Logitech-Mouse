398c0dd761f60a8b4d54ddc5f2d1ea3865d6be78
ForeGX <107768580+ZharTX@users.noreply.github.com>
2024-06-03 09:03:32 +0700

    Update world.lua

--- world.lua
+++ world.lua
@@ -6,7 +6,20 @@
 --| macro is working on your PC.                                              |--
 ---------------------------------------------------------------------------------
 
-NONE
+if (event == "PROFILE_ACTIVATED") then end
+EnablePrimaryMouseButtonEvents(true)
+  function OnEvent(event, arg)
+    if IsMouseButtonPressed(4) then
+      repeat -- foregx.com
+        if IsMouseButtonPressed(1) then
+          repeat
+          MoveMouseRelative(0, 5)
+          Sleep(6) -- foregx.com
+          until not IsMouseButtonPressed(1)
+        end
+      until not IsMouseButtonPressed(1)
+    end
+end
 
 --| If this macro works on your pc, you can buy the paid version
 --| Contact Me for Purchase VIP Macro 99% No Recoil
